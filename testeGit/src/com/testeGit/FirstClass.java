251ea98588b827110d3080a9aee87b3124b61166
rafaelsoares <rafaelsoares@.betboo.com>
2012-11-26 11:49:40 -0300

    Comitando terceira classe

---
+++ testeGit/src/com/testeGit/FirstClass.java
@@ -0,0 +1,11 @@
+package com.testeGit;
+
+public class FirstClass {
+	
+	public static void main(String[] args) {
+		System.out.println("First class");
+		System.out.println("Another sysout message");
+		System.out.println("Tentativa de push");
+	}
+
+}
---
+++ testeGit/src/com/testeGit/SecondClass.java
@@ -0,0 +1,8 @@
+package com.testeGit;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		
+	}
+}
